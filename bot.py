0cea23d4b7c882f9d1a75d54b738edf4990e8fd6
Liam Murphy <lmur451@aucklanduni.ac.nz>
2021-08-17 21:35:54 +1200

    Added check on bot startup

--- bot.py
+++ bot.py
@@ -18,9 +18,32 @@ class DriftBot(discord.Client):
     async def on_ready(self):
         print("Logged on as {0}".format(self.user))
         self.server = self.get_guild(SERVER_ID)
+        is_playing = False
+        for member in self.server.members:
+            for activity in activities_of_interest:
+                if activity in [b.name for b in member.activities]: is_playing = True
+            if is_playing:
+                role = self.role_reserve(member)
+                await member.add_roles(role)
+            else:
+                await member.remove_roles(role)
 
     async def on_message(self, message):
         print("Message from {0.author}: {0.content}".format(message))
+
+    def role_resolve(self, member):
+        player_roles_ids = [role.id for role in member.roles]
+
+        if CREW_ID in player_roles_ids:
+            role_to_change = CREW_PLAY_ID
+        elif REGULAR_ID in player_roles_ids:
+            role_to_change = REGULAR_PLAY_ID
+        else:
+            role_to_change = PLAY_ID
+
+        role = self.server.get_role(role_to_change)
+
+        return role
 
     async def on_member_update(self, before, after):
         was_playing = False
@@ -34,16 +57,7 @@ class DriftBot(discord.Client):
             #Do nothing
             return
 
-        player_roles_ids = [role.id for role in after.roles]
-
-        if CREW_ID in player_roles_ids:
-            role_to_change = CREW_PLAY_ID
-        elif REGULAR_ID in player_roles_ids:
-            role_to_change = REGULAR_PLAY_ID
-        else:
-            role_to_change = PLAY_ID
-
-        role = self.server.get_role(role_to_change)
+        role = self.role_resolve(after)
 
         if not was_playing and is_playing:
             await after.add_roles(role)
